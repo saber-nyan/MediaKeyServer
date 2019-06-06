ebb25ba67a643cf666506166401d0e200a5051a3
saber-nyan <saber-nyan@ya.ru>
2019-06-06 13:24:29 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace KeyServer {
+	internal static class Program {
+		private const int KeyeventfExtendedkey = 0x0001; //Key down flag
+		private const int KeyeventfKeyup = 0x0002; //Key up flag
+
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+		/**
+		 * Keycodes: https://docs.microsoft.com/ru-ru/windows/desktop/inputdev/virtual-key-codes
+		 * Before starting, execute as admin (for russian locale): netsh http add urlacl url=http://+:8089/ user=Все
+		 */
+		public static void Main(string[] args) {
+			Console.WriteLine("Starting server...");
+			if (!HttpListener.IsSupported) {
+				Console.WriteLine("Server is not supported, aborting!");
+				return;
+			}
+
+			const string listenAddress = "http://+:8089";
+			var server = new HttpListener();
+			server.Prefixes.Add($"{listenAddress}/pressKey/");
+			server.Start();
+			Console.WriteLine("Server started!");
+
+			while (true) {
+				var context = server.GetContext();
+				var request = context.Request;
+				var requestString = new StreamReader(request.InputStream).ReadToEnd();
+
+				Console.WriteLine($"Got request {requestString}");
+
+				var response = context.Response;
+				byte[] responseBuffer;
+				try {
+					var json = JObject.Parse(requestString);
+					var keyName = Convert.FromBase64String(json["key"].ToString())[0];
+					Console.WriteLine($"Pressing key '{keyName}'...");
+					keybd_event(keyName, 0, KeyeventfExtendedkey, 0);
+					keybd_event(keyName, 0, KeyeventfKeyup, 0);
+
+
+					var oJson = new JObject {
+						["success"] = true
+					};
+					responseBuffer = Encoding.UTF8.GetBytes(oJson.ToString());
+				}
+				catch (Exception e) {
+					Console.WriteLine($"Error while parsing: {e}");
+					var oJson = new JObject {
+						["success"] = false,
+						["errorName"] = e.GetType().ToString(),
+						["errorDescription"] = e.ToString()
+					};
+					responseBuffer = Encoding.UTF8.GetBytes(oJson.ToString());
+				}
+
+				response.ContentLength64 = responseBuffer.Length;
+
+				var outStream = response.OutputStream;
+				outStream.Write(responseBuffer, 0, responseBuffer.Length);
+				outStream.Close();
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KeyServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Home, desu~")]
+[assembly: AssemblyProduct("KeyServer")]
+[assembly: AssemblyCopyright("Copyleft saber-nyan © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6F4613A1-2FC5-4D78-B275-525254D5C211")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
